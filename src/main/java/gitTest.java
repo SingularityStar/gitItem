aa357e4d254d45d71c5c3529cff0d9b8de236e75
dongfangbubai <linghu@yingying.com>
2021-04-15 22:35:11 +0800

    hot-fix commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,5 +1,7 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello hot-fix");
+        System.out.println("冲突hot-fix");
     }
 }
